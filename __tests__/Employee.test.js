4ebdafceed3079112089d3c681b7acc67c3f91b2
Preston Fassbender <fassbenderp0551@gmail.com>
2022-02-06 11:44:18 -0600

    created employee class to pass employee tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,35 +1,33 @@
-const { expect } = require("@jest/globals");
-const { test } = require("picomatch");
 const Employee = require("../lib/Employee");
 
 test("creates an employee object", () => {
     const employee = new Employee("Nick", 123, "nick@email.com");
 
-    expect(employee.name).toBe("Nick");
+    expect(employee.name).toEqual("Nick");
     expect(employee.id).toBe(123);
-    expect(employee.email).toBe("nick@email.com");
+    expect(employee.email).toEqual("nick@email.com");
 });
 
 test("gets the employee name", () => {
     const employee = new Employee("Nick", 123, "nick@email.com");
 
-    expect(employee.getName()).toReturnWith("Nick");
+    expect(employee.getName()).toEqual("Nick");
 });
 
 test("gets the employee id", () => {
     const employee = new Employee("Nick", 123, "nick@email.com");
 
-    expect(employee.getId()).toReturnWith(123);
+    expect(employee.getId()).toBe(123);
 });
 
 test("gets the employee email", () => {
     const employee = new Employee("Nick", 123, "nick@email.com");
 
-    expect(employee.getEmail()).toReturnWith("nick@email.com");
+    expect(employee.getEmail()).toEqual("nick@email.com");
 });
 
 test("get the employee role", () => {
     const employee = new Employee("Nick", 123, "nick@email.com");
 
-    expect(employee.getRole()).toReturnWith("Employee");
+    expect(employee.getRole()).toEqual("Employee");
 });
